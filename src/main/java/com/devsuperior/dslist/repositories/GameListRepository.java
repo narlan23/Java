b3cf346259edda4442f1a70200e863b4a5c55678
Narlan Mota <narlanmota19@gmail.com>
2023-07-14 14:56:01 -0300

    GET lists

--- src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameListRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.*;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.entities.GameList;
 
-public interface GameRepository extends JpaRepository<Game,Long> {
+public interface GameListRepository extends JpaRepository<GameList,Long> {
 	
 
 }
